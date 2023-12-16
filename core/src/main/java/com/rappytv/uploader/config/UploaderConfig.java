0e60d98ffe8b6ebcc605a8836f0bba6f078f514a
RappyTV <rappytv@rappytv.com>
2023-12-16 23:00:59 +0100

    Add config spritesheet

--- core/src/main/java/com/rappytv/uploader/config/UploaderConfig.java
+++ core/src/main/java/com/rappytv/uploader/config/UploaderConfig.java
@@ -7,17 +7,24 @@ import com.rappytv.uploader.config.subconfig.ZiplineSubconfig;
 import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.dropdown.DropdownWidget.DropdownSetting;
+import net.labymod.api.configuration.loader.annotation.SpriteSlot;
+import net.labymod.api.configuration.loader.annotation.SpriteTexture;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 import net.labymod.api.configuration.settings.annotation.SettingSection;
 
+@SpriteTexture("settings")
 public class UploaderConfig extends AddonConfig {
 
+    @SpriteSlot(size = 32)
     @SwitchSetting
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
+    @SpriteSlot(size = 32, x = 1)
     @DropdownSetting
     private final ConfigProperty<Uploaders> uploader = new ConfigProperty<>(Uploaders.ZIPLINE);
     @SettingSection("uploaders")
+    @SpriteSlot(size = 32, y = 1)
     private final EShareSubconfig eshare = new EShareSubconfig();
+    @SpriteSlot(size = 32, y = 1, x = 1)
     private final ZiplineSubconfig zipline = new ZiplineSubconfig();
 
     @Override
--- core/src/main/java/com/rappytv/uploader/config/subconfig/EShareSubconfig.java
+++ core/src/main/java/com/rappytv/uploader/config/subconfig/EShareSubconfig.java
@@ -1,12 +1,8 @@
 package com.rappytv.uploader.config.subconfig;
 
-import net.labymod.api.Laby;
-import net.labymod.api.client.component.Component;
 import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget.TextFieldSetting;
 import net.labymod.api.configuration.loader.Config;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
-import net.labymod.api.notification.Notification;
-import net.labymod.api.util.Debounce;
 
 public class EShareSubconfig extends Config {
 
