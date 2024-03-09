c1f88ef9bc08c475a6078f4dd2eeaaa278764775
RappyTV <rappytv@rappytv.com>
2024-03-09 18:53:07 +0100

    Add more zipline options to ZiplineSubconfig

--- core/src/main/java/com/rappytv/uploader/config/subconfig/ZiplineSubconfig.java
+++ core/src/main/java/com/rappytv/uploader/config/subconfig/ZiplineSubconfig.java
@@ -1,10 +1,16 @@
 package com.rappytv.uploader.config.subconfig;
 
+import com.rappytv.uploader.util.ZiplineNameFormat;
 import net.labymod.api.Laby;
 import net.labymod.api.client.component.Component;
+import net.labymod.api.client.gui.screen.widget.widgets.input.SliderWidget.SliderSetting;
+import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget.TextFieldSetting;
+import net.labymod.api.client.gui.screen.widget.widgets.input.dropdown.DropdownWidget.DropdownSetting;
 import net.labymod.api.configuration.loader.Config;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
+import net.labymod.api.configuration.settings.annotation.SettingRequires;
+import net.labymod.api.configuration.settings.annotation.SettingSection;
 import net.labymod.api.notification.Notification;
 import net.labymod.api.util.Debounce;
 
@@ -34,11 +40,38 @@ public class ZiplineSubconfig extends Config {
     private final ConfigProperty<String> base = new ConfigProperty<>("https://example.org");
     @TextFieldSetting
     private final ConfigProperty<String> auth = new ConfigProperty<>("");
+    @SettingSection("settings")
+    @TextFieldSetting
+    private final ConfigProperty<String> domains = new ConfigProperty<>("");
+    @SettingRequires(value = "zeroWidth", invert = true)
+    @DropdownSetting
+    private final ConfigProperty<ZiplineNameFormat> nameFormat = new ConfigProperty<>(ZiplineNameFormat.DEFAULT);
+    @SwitchSetting
+    private final ConfigProperty<Boolean> zeroWidth = new ConfigProperty<>(false);
+    @SwitchSetting
+    private final ConfigProperty<Boolean> embed = new ConfigProperty<>(true);
+    @SliderSetting(min = 0, max = 100, steps = 10f)
+    private final ConfigProperty<Integer> compression = new ConfigProperty<>(0);
 
     public String base() {
         return base.get();
     }
     public String auth() {
         return auth.get();
+    }
+    public String domains() {
+        return domains.get();
+    }
+    public ZiplineNameFormat nameFormat() {
+        return nameFormat.get();
+    }
+    public boolean zeroWidth() {
+        return zeroWidth.get();
+    }
+    public boolean embed() {
+        return embed.get();
+    }
+    public int compression() {
+        return compression.get();
     }
 }
