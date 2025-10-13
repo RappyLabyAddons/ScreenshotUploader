74bd30aa17f3301a4f612447a396533ba19cd1ce
RappyTV <rappytv@rappytv.com>
2025-10-14 01:54:23 +0200

    chore: Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
 
 group = "org.example"
-version = providers.environmentVariable("VERSION").getOrElse("1.0.4")
+version = providers.environmentVariable("VERSION").getOrElse("1.0.5")
 
 labyMod {
     defaultPackageName = "com.rappytv.screenshotuploader"
